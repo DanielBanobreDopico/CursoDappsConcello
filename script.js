f9fad64d8bcd579732f567d5603eb1ed2e1af187
Daniel Banore Dopico <daniel.banobre.dopico@protonmail.com>
2021-05-27 19:18:42 +0200

    Corrección para poder emplear etiquetas HTML en la salida. Botón "limpiar". Algo de decoración

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
+const palabraProhibida = "calamar";
+
 let o = null;
+let i = null;
+let b = null;
 
 function main () {
-    let i = document.querySelector('#areaTexto');
+    i = document.querySelector('#areaTexto');
     o = document.querySelector('#vistaTexto');
+    let b = document.querySelector('#clearButton');
     i.addEventListener('input',inputHandler);
+    b.addEventListener('click',clearTextAreas);
+    i.placeholder = `Escribe algo, pero no escribas "${palabraProhibida}".`;
 }
 
 function inputHandler (ev) {
-    o.value = ev.target.value//.replace(/\n/,'<br>');
+    const text = i.value.replace(palabraProhibida,`<strong>${palabraProhibida}</strong>`);
+    console.log(text)
+    o.innerHTML = text;
+    console.log(o.innerHTML)
+}
+
+function clearTextAreas () {
+    i.value = "";
+    o.innerHTML = "";
 }
 
 document.addEventListener('readystatechange',main);
