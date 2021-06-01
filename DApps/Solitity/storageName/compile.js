51eb30732e9f60f13849f10605e1c1f7d4ac5510
Daniel Banore Dopico <daniel.banobre.dopico@protonmail.com>
2021-06-01 19:41:45 +0200

    Print abi

--- DApps/Solitity/storageName/compile.js
+++ DApps/Solitity/storageName/compile.js
@@ -5,4 +5,25 @@ const solc = require('solc');
 const storageName = path.resolve(__dirname, 'contracts', 'storageName.sol');
 const source = fs.readFileSync(storageName, 'utf8');
 
-console.log(source);
+//console.log(source);
+
+const input = {
+    language: 'Solidity',
+    sources: {
+        'storageName.sol': {
+            content: source,
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*']
+            }
+        }
+    }
+}
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+//console.log(output);
+console.log(output.contracts['storageName.sol'].StorageName.abi);
